78a0b43544fe33f7e64f004171d31dd71b2db804
Venky490 <venky.javadeveloper490@gmail.com>
2023-11-11 13:05:28 +0530

    first commit

---
+++ src/main/java/com/example/employemenagement/EmployemenagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.employemenagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployemenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployemenagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employemenagement/EmployemenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employemenagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployemenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
